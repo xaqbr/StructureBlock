4760a2b3ff61500e887d91f930467f1b9352d20c
Wouter B <33959438+WouterB15@users.noreply.github.com>
2024-06-30 15:43:00 +0200

    Replaced block placement trigger

--- src/main/java/net/wouterb/structureblock/mixin/item/BlockItemMixin.java
+++ src/main/java/net/wouterb/structureblock/mixin/item/BlockItemMixin.java
@@ -1,9 +1,8 @@
 package net.wouterb.structureblock.mixin.item;
 
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.registry.Registries;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
@@ -14,12 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemStack.class)
-public class ItemStackMixin {
+@Mixin(BlockItem.class)
+public class BlockItemMixin {
 
-
-    @Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
-    public void useOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> ci) {
+    @Inject(method = "place", at = @At("HEAD"), cancellable = true)
+    public void place(ItemPlacementContext context, CallbackInfoReturnable<ActionResult> ci) {
         String stackId = Registries.ITEM.getId(context.getStack().getItem()).toString();
 
         PlayerEntity player = context.getPlayer();
